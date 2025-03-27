47ac2c6a7627b2f20ecf91d33bb25bf1c5c920ad
Yumeng Ji <yumeng.ji103585@gmail.com>
2025-03-27 13:02:05 +0100

    Create CasillaMadre.java

---
+++ CasillaMadre.java
@@ -0,0 +1,11 @@
+public class CasillaMadre{
+  private int idCasilla;
+  private String textoImprimir = "Has caído en una casilla normal.";
+  
+public CasillaMadre(int pIdCasilla){
+  this.idCasilla = pIdCasilla;
+}
+
+public void realizarAccion(Jugador pJugador){
+}
+  
