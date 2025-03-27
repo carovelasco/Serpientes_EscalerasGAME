ceb93096b7aa4c461b82bde3e47524d1420942a4
Yumeng Ji <yumeng.ji103585@gmail.com>
2025-03-27 13:23:26 +0100

    Update CasillaEspecial.java

--- CasillaEspecial.java
+++ CasillaEspecial.java
@@ -2,9 +2,11 @@ public class CasillaEspecial{
   private int idCasillaDestino;
   private String textoImprimir;
   
-public CasillaMadre(int pIdCasillaDestino){
+public CasillaMadre(int pIdCasillaDestino, String pTextoImprimir){
   this.idCasillaDestino = pIdCasillaDestino;
+  
 }
+
 
 public void realizarAccion(Jugador pJugador){
   
