3035ca98216fbc03246a9c67260e3ebd40c9ebe0
sofi427 <maginscreations@gmail.com>
2025-04-07 15:53:40 +0200

    Update and rename CasillaCasiFin to CasillaCasiFin.java

---
+++ CasillaCasiFin.java
@@ -0,0 +1,22 @@
+public class CasillaCasiFin extends CasillaMadre
+{
+  private int idCasillaDestino;
+  private String textoImprimir;
+  
+  public CasillaEspecial(int pIdCasillaDestino, String pTextoImprimir) {
+      super(0); // El ID de la casilla se asigna en el tablero
+      this.idCasillaDestino = pIdCasillaDestino;
+      this.textoImprimir = pTextoImprimir;
+  }
+  
+  @Override
+  public void realizarAccion(Jugador pJugador) {
+
+        //hay que haber sacado un 6, se necesita pValorDado
+      System.out.println(textoImprimir);
+      pJugador.setIdCasillaPosicion(this.idCasillaDestino);
+  }
+  public int getIdCasillaDestino() {
+    return idCasillaDestino;
+  }
+}
