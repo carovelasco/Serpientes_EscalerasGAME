3759fdaa2846a120a18f0f4f10df63c2436d9779
Samira034 <samira.tamang247@gmail.com>
2025-04-26 20:11:47 +0200

    Update Dado.java

--- Dado.java
+++ Dado.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Dado {
     private static Dado miDado;
     private Random random; 
+    private int valorFijado =-1;
 
     private Dado() {
         random = new Random(); 
@@ -16,7 +17,21 @@ public class Dado {
     }
 
     public int lanzarDado() {
+        if(this.valorFijado!=-1)
+        {
+            return valorFijado;
+        }
         return random.nextInt(6) + 1; 
+    }
+
+    //Para Pruebas
+    public void setValorDado(int Valor)
+    {
+        this.valorFijado= Valor;
+    }
+    public static void reiniciar()
+    {
+        miDado=null;
     }
 
 }
